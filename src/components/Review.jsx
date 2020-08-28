da517091fbc90ab4ddfa9f95f6f4f027baf2230c
Ken Ingram <ken.ingram@gmail.com>
2020-08-28 14:18:20 -0700

    Add delete functionality

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 import UpdateReview from './UpdateReview';
 
 function Review (props){
 	const { author, title, text} = props.review.fields;
 	const { fetchReviews, setFetchReviews, review } = props;
+
+	async function handleDelete(){
+		const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/frylife/${review.id}`;
+		// Make the delete request
+		await axios.delete(airtableURL, {
+			headers: {
+				'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+			}
+		});
+		// Refresh the reviews from the database
+		setFetchReviews(!fetchReviews);
+
+	}
+
 
 	return (
 		<div>
@@ -14,7 +29,8 @@ function Review (props){
 				fetchReviews={fetchReviews}
 				setFetchReviews={setFetchReviews}
 				review={review}
-			/>			
+			/>
+			<button onClick={handleDelete}>Delete!</button>
 		</div>
 	)
 }
