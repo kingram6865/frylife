a0b8d81095fc7a8b6747159c22eed57d02bacc37
Ken Ingram <ken.ingram@gmail.com>
2020-09-01 08:08:13 -0700

    Changes from morning lecture 2020 09 01

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect }  from 'react';
 import axios from 'axios';
 import CreateReview from './components/CreateReview';
 import Review from './components/Review';
+import { baseURL } './services/constants'
 import './App.css';
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
     // Cannot run async functions directly in useEffect
     // console.log('component has mounted!');
     const getReviews = async () => {
-      const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/frylife?Grid%20View`;
+      //const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/frylife?Grid%20View`;
+      const airtableURL = `${baseURL}?Grid%20View`;
       const response = await axios.get(airtableURL, {
         headers: {
           'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
